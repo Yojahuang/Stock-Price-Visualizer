56c2dcab48c0089e2f7314cee61c4d18def16286
Yojahuang <yojahuang30613@gmail.com>
2022-06-22 00:29:15 +0800

    Use tailwind css and shared folder

---
+++ frontend/src/share/shared.js
@@ -0,0 +1,11 @@
+let homepageURL = "http://localhost:8080"
+let apiBaseURL = "http://10.4.2.110:5000"
+
+export default {
+    getHomepageURL: function() {
+        return homepageURL
+    },
+	getAPIBaseURL: function() {
+		return apiBaseURL
+	}
+}
