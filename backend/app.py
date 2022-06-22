9dd6b6d52c550f7f1c17953d049f5026fb8f56c9
Yojahuang <yojahuang30613@gmail.com>
2022-06-22 16:32:27 +0800

    feature: support CORS

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 import config
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = config.databaseURL
 db = SQLAlchemy(app)
 
